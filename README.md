c650149a566f10dcf6beeb91b4a32c5d74af01e9
Elijah Cruz <elijah@ecwebservices.net>
2021-01-16 22:29:21 -0800

    Initial Commit, possible only one of two commits.

---
+++ README.md
@@ -0,0 +1,3 @@
+# ELO Rating Generating Class
+
+This is a simple class that allows you to generate an ELO rating for two chess players after a match. Defaults are set to make things even easier. It is also one Class file as well, so check out the source to see how simple it is to work with. 
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'elijahcruz/elo-rating',
+  ),
+  'versions' => 
+  array (
+    'elijahcruz/elo-rating' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
